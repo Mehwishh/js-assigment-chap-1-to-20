633a276dd92760fbba6e10906f98edbaa66fdad3
Mehwish Hussain <65733875+Mehwishh@users.noreply.github.com>
2020-09-06 11:38:14 -0700

    add

---
+++ app.js
@@ -0,0 +1,241 @@
+                 // ===== chapter 1 =======
+// Task 1
+// alert ("Error!" + " Please enter a valid password")
+// Task 2
+// alert ("Welcome to Js Land"+ "\n" +"Happy Coding!")
+// Task 3
+// alert ("Welcome to Js Land")
+// Task 4
+// alert ("Happy coding") 
+// Task 5
+// confirm ("Prevent this page from creating additional dialogs")
+// Task 6
+// console.log ("Hello..." + "I can run js through my web browser console")
+        
+                // ===== chapter 2 =======
+// task 0            
+//   var username = prompt ("ENTER YOUR NAME ")       
+//   var myname   = "mehwish" 
+//   var message ='Hello World'
+//   alert (message)
+// task 1
+//** student bio data** 
+ 
+// var name =" john"
+// var age = 17
+// var course = "certified mobile application development"
+// alert (name)
+// alert(age)
+// alert(course)
+
+
+// script display pizza 
+// task 2
+// var var1 = "pizza";
+// var var2 = "pizz";
+// var var3 = "pi";
+// var var4 = "p";
+// alert (var1 + "\n" + var2 + "\n" + var3 + "\n" + var4 )
+
+//  task 3
+//  email = "mehwishh165@gmail.com"
+//  alert ("MY email adress is "  + email)
+
+//  task 4 
+// var book = "A smartest way to learn javascript";
+// alert( "I am trying to learn from book " + book)
+
+
+// task 5
+// var a = " Yes I can write HTMl content through js"
+// document.write(a)
+
+//  Task 6
+// var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert (end)
+
+
+
+
+// ======= chapter 3===========
+//  tak 1
+//  var age = 15;
+//  alert("I am" + age  + " year old");
+//  tak 2
+//  var n = 14;
+//  alert("You have visited this site " + n +  " time")
+//  task 3
+//  var birthyear =( 1990);
+//  document.write ("My birth year is " + birthyear  +"<br> <br> <br>"   +  "  Data type of my declared variable is number")
+//  task 4
+//  var visitorname = ("John Doe");
+//  var producttitle = (" T-Shirts");
+//  var quantity =(5);
+//  document.write ( visitorname + '  ordered ' + quantity  +  producttitle  + " on xyz clothing store")
+// chapter 4
+// task 1
+// var var1 = 222 , var2 = 445 , var3 = 665; 
+// legal
+// var FirstName ="";
+// var $ = "";
+// var percent = "";
+// var my_value = "";
+// var _data = "";
+// inlegal
+// var 29 ="";
+// var @gmail ="";
+// var the-end = "";
+// var #265 = "";
+// var -abc = '';
+
+// // task 1
+// var rules = ("<h1> Rules For  Naming Js Variables</h1>")
+// var rule1 = ("<h4>  variable name can only contain numbers, variables,$ and _ For Example: <b>smy_1stvariable</b></h4>")
+// var rule2 = ("<h4> Variable must begin with a  letter  $ or _For Example:<b>$name, _name or name</b>  </h4>")
+// var rule3 = ("<h4> Variable names should not be JS  Keyword </h4>")
+// var rule4 = ("<h4></h4>")
+// document.write (rules  + rule1  + rule2  + rule3)
+
+// chapter 5
+// task 1
+// var a = 3;
+// var b = 5;
+// var c = (a-b)
+// document.write ("The sum of 3 + 5 is " + c )
+// // task 1\2
+// var a = 3;
+// var b = 5;
+// var c = (a+b)
+// document.write ("The sum of 3 + 5 is " + c )
+// task 3
+// var a = 3;
+// var b = 5;
+// var c = (a*b)
+// document.write ("The sum of 3 + 5 is " + c )
+// // task 4
+// var a = 3;
+// var b = 5;
+// var c = (a/b)
+// document.write ("The sum of 3 + 5 is " + c )
+// // task 5
+// var a = 3;
+// var b = 5;
+// var c = (a%b)
+// document.write ("The sum of 3 + 5 is " + c )
+
+// var var1 = 5;
+// var var2 =  ++var1 ;
+// var var3 =   ++var1 + 7; 
+// var var4 =    --var1 + 7 ;
+// var var5 =       var4 % 3;   
+// document.write( "value of variable after variable declaration is: undefine" + "</br>" )
+// document.write (" initial value  is :" +  var1 + "</br>")
+// document.write (" after increment is : " +  var2  +"</br>" )
+// document.write (" afte addition  is : " + var3 + "</br>" )
+// document.write (" after decrement   is : " + var4 + "</br>" )
+// document.write (" afte reminder value is : " + var5 + "</br>" )
+// // TASK 2
+// var cost =(600 );
+// var Ticket5 = (cost*5)
+// document.write ( "Totle cost to buy a movie tickets is " + Ticket5 + " PKR")
+// Task 3
+ // var a = 4
+// document.write("4 * 1 = " + a + "<br>")
+// document.write("4 * 2 = " + a*2 + "<br>")
+// document.write("4 * 3 = " + a*3 + "<br>")
+// document.write("4 * 4 = " + a*4 + "<br>")
+// document.write("4 * 5 = " + a*5 + "<br>") 
+// document.write("4 * 6 = " + a*6 + "<br>")
+// document.write("4 * 7 = " + a*7 + "<br>")
+// document.write("4 * 8 = " + a*8 + "<br>")
+// document.write("4 * 9 = " + a*9 + "<br>")
+// document.write("4 * 10 = " + a*10 + "<br>")
+// Task 
+// var C = 25 ;
+// var F = (25*9/5) + 32;
+// var Frh = 70;
+
+// document.write (C +"<sup>0</sup>C" + " is " + F +"<sup>o</sup>F" + "</br>" );
+// document.write  (Frh +"<sup>0</sup>F" + " is " + ( Frh -32)*5/9  +"<sup>O</sup>C" );
+
+// TASK 3
+// var price = 650
+// var quantity = 3
+// var price2 = 100
+// var quantity2 = 7
+// var charges = 100
+// var cost = (650 +3+100+7+100)
+// document.write("<h1>Shopping Cart</h1>" + "<br>")
+// document.write("Price of  item1 is "+ price + "<br>")
+// document.write("Quantity of item1 is "+ quantity + "<br>")
+// document.write("Price of item2 is "+ price2 + "<br>")
+// document.write("Quantity of item2 is "+ quantity2 + "<br>"+ "<br>")
+// document.write ("Total cost of your order is " + cost)
+//  Task 4
+// var marks = 980
+// var obt_marks = 804
+// document.write("<h1>Marks Sheet</h1>")
+// document.write("Total marks: "+ marks + "<br>")
+// document.write ("Obtained  Marks:  " + obt_marks  + "<br>" )
+// document.write ("Percentage: " + (804/980*100)+"%")
+// task 5
+// var dollor = 10;
+//     ryal = 25;
+//     $_t0_PKR = 104.80;
+//     PKR_to_ryal = 28
+//     document.write ( "<h1>Currency In PKR </h1>"+"<br>"+"Total Currency in PKR is: "+(dollor*$_t0_PKR+25*PKR_to_ryal))   
+// Task 6
+// var num = 99
+//     answer = ((99+5*10)/2)
+//     document.write(answer)
+// TASK 7 (AGE CALCULATOR)
+// var current_year = 2020;
+//     Birth_year = 2004;
+//     age = current_year-Birth_year;
+//     document.write("<h1>Age Calculator </h1>")
+//     document.write ("Current year: " + current_year + "<br>" + "Birth year: " + Birth_year + "<br>" + "Your Age: " + age)
+
+// task 8
+// var radius = 20;
+//     circumference = 2*3.142*20
+//     area = 3.142*20*20
+//     document.write("<h1> The Geometrizer</h1>")
+//     document.write("Radius of circle: " + radius + "<br>" + "The cirumference is: " + circumference + "<br>"+ "The area is: " + area)
+
+// task 9
+//  var fav_snack = "lays";
+//      age = 15;
+//      estimated_age = 65;
+//      per_day = 3;
+//      total= (65-15)*3;
+//      document.write("<h1> The Lifetime Supply Calculator </h1>")
+//      document.write("Favourit Snacks: " + fav_snack +"<br>"  + "Current age: " + age +"<br>"  + "Estimated maximum age; " + estimated_age + "<br>" + "Amount of snacks per day: " + per_day+"<br>"  + "You will need "+ total +" to last you until the ripe old age of " + estimated_age)
+
+// ======chapter6=====
+// var a =10;
+// document.write("<h3>Results</h3>")
+// document.write("The value of a is " +a)
+// document.write("<br>..................................")
+//     a = ++a
+//     document.write("<br>"+"The value of a afer ++a is " ,  +a)
+//     document.write("<br> Now the value of  a is : " +a);
+//     a = a++
+//     document.write("<br>"+"<br>"+ "The value of  a++ is " ,  +a)
+//     document.write("<br> Now the value of  a is : " +a );
+//     a = --a 
+//     document.write("<br>"+"<br>"+ "The value of --a is " ,  +a)
+//     document.write("<br> Now the value of  a is : " +a);
+//     a = a-- 
+//     document.write("<br>"+"<br>"+ "The value of a-- is " ,  +a)
+//     document.write("<br> Now the value of  a is : " +a);
+// var a = 2, b = 1;
+
+//     document.write ( " vakue of a is : " + a  + "<br>")
+//     document.write( " value of b is: "+ b + "<br>" )
+// var result = --a - --b + ++b +  b--  
+            // --2  - --1 +   ++1 + 1--   
+            // -1 + 2 -   1-1  +  1+1 + 1 -1
+            //    1      - 0  +    2    + 0
+//  document.write("result is " + result)
+// (greeting msg)
+var 
